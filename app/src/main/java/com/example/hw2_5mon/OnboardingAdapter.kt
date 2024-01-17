6f3aa24ab4ad2bb0dfc6408b65e5d986ed80a3cf
asant <asantursunaliev0606@gmail.com>
2024-01-17 16:26:22 +0300

    hw_5

--- app/src/main/java/com/example/hw2_5mon/OnboardingAdapter.kt
+++ app/src/main/java/com/example/hw2_5mon/OnboardingAdapter.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class OnboardingAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
+class OnboardingAdapter (fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
 
     private val onboardingSlides = listOf(
         OnboardingFragment.newInstance("Title 1", "Desc 1"),
